583a7223758d930559c45706ce2246ad8705f7e7
unknown <dam0914@gmail.com>
2021-04-13 19:54:27 +0200

    test of cicd to dev v3

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', {
-    title: 'test of CICD with slot',   
+    title: 'Welcome to Contoso!!!',   
   });
 });
 
